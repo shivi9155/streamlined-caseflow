8850cb9de002b0d6fbaef715159a3330a94b6adf
06krish <143773425+06krish@users.noreply.github.com>
2025-12-17 00:12:37 +0530

    login/signup page added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import CaseManagement from './pages/CaseManagement';
 import Layout from './components/Layout';
 import Analytics from "./pages/analytics";
 import Parties from "./pages/parties";
+import Authpage from "./pages/Authpage";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -18,14 +19,15 @@ function App() {
       <div className={darkMode ? 'dark' : ''}>
         <Routes>
           <Route path="/" element={<Home darkMode={darkMode} toggleDarkMode={toggleDarkMode} />} />
+          <Route path="/auth" element={<Authpage />} />
           <Route path="/cases" element={
             <Layout darkMode={darkMode} toggleDarkMode={toggleDarkMode}>
               <CaseManagement />
             </Layout>
           } />
-          <Route path="*" element={<Navigate to="/" />} />
           <Route path="/analytics" element={<Analytics />} />
-            <Route path="/parties" element={<Parties />} />
+          <Route path="/parties" element={<Parties />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </div>
     </Router>
